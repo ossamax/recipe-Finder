125e36d84f525cc76592e3f1499ffe9e05cbdeea
ossama zaoute <o.zaoute@intel-think.com>
2024-04-08 16:04:33 +0000

    testing api fetching

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,34 @@
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { onMounted, ref, watchEffect } from "vue";
+// import { data } from "../data";
+
+const inputValue = ref("");
+const data = ref([]);
+function GetData() {
+  axios
+    .get("https://api.spoonacular.com/recipes/findByIngredients", {
+      params: {
+        ingredients: inputValue.value ? inputValue.value : "chicken,pasta,drinks",
+        apiKey: "17cbab47fdf64d43b4e9f7661dd7ea66",
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      data.value = response.data;
+    })
+    .catch((error) => {
+      this.error = error; // Store error in error variable
+      console.error("Error fetching recipes:", error);
+      this.loading = false; // Set loading state to false
+    });
+}
+
+onMounted(GetData);
+watchEffect(() => {
+  GetData();
+}, [inputValue]);
+</script>
 
 <template>
   <main>
@@ -29,8 +59,9 @@
           ></span>
           <input
             type="search"
-            name=""
-            id=""
+            name="search"
+            id="search"
+            v-model="inputValue"
             placeholder="What do you want to cook today?"
           />
         </div>
@@ -95,9 +126,33 @@
         </div>
       </div>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li v-for="(item, index) in data" :key="index">
+          <div class="likes-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 256 256"
+            >
+              <path
+                fill="currentColor"
+                d="M178 36c-20.09 0-37.92 7.93-50 21.56C115.92 43.93 98.09 36 78 36a66.08 66.08 0 0 0-66 66c0 72.34 105.81 130.14 110.31 132.57a12 12 0 0 0 11.38 0C138.19 232.14 244 174.34 244 102a66.08 66.08 0 0 0-66-66m-5.49 142.36a328.69 328.69 0 0 1-44.51 31.8a328.69 328.69 0 0 1-44.51-31.8C61.82 159.77 36 131.42 36 102a42 42 0 0 1 42-42c17.8 0 32.7 9.4 38.89 24.54a12 12 0 0 0 22.22 0C145.3 69.4 160.2 60 178 60a42 42 0 0 1 42 42c0 29.42-25.82 57.77-47.49 76.36"
+              /></svg
+            >{{ item.likes }}
+          </div>
+          <div class="img-bg">
+            <img :src="item.image" alt="image" class="card_img" />
+          </div>
+          <div class="content">
+            <h3>{{ item.title }}</h3>
+            <!-- <div class="ingredients-list">
+              <div v-for="(ingredient, key) in item.usedIngredients" :key="key">
+                {{ ingredient.name }}
+              </div>
+            </div> -->
+            <button>View Ingredients</button>
+          </div>
+        </li>
       </ul>
       <div class="intro">
         <div class="intro_head">
@@ -211,7 +266,7 @@
   width: 80%;
   padding: 2rem;
   max-width: 1400px;
-  margin: auto;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   gap: 40px;
@@ -243,7 +298,7 @@
 }
 
 main {
-  height: 100%;
+  min-height: 100vh;
   display: flex;
   gap: 20px;
 }
@@ -285,10 +340,77 @@ ul {
   gap: 20px;
   li {
     background-color: white;
-    padding: 1rem;
-    border-radius: 20px;
-    height: 180px;
+    border-radius: 10px;
+    position: relative;
+    overflow: hidden;
     box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
+    .likes-icon {
+      background-color: rgb(252, 50, 83);
+      color: white;
+      border-radius: 15px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 5px;
+      position: absolute;
+      top: 1rem;
+      right: 1rem;
+      width: max-content;
+      padding: 0 1rem;
+      height: 35px;
+      z-index: 100;
+    }
+    .content {
+      padding: 1rem;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+    }
+    .img-bg {
+      position: relative;
+      height: 150px;
+      &::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background-color: rgba(0, 0, 0, 0.103);
+        z-index: 2;
+      }
+    }
+    .card_img {
+      object-fit: cover;
+      width: 100%;
+      z-index: 1;
+      height: 100%;
+    }
+    h3 {
+      color: black;
+    }
+    .ingredients-list {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      div {
+        background-color: #9caebd36;
+        color: #28455b;
+        border-radius: 10px;
+        padding: 0.3rem 1rem;
+        font-size: 0.8rem;
+      }
+    }
+    button {
+      width: 100%;
+      height: 45px;
+      background-color: rgb(248, 152, 43);
+      color: white;
+      border-radius: 20px;
+      font-weight: 600;
+      border: none;
+      cursor: pointer;
+      &:hover {
+        background-color: rgb(223, 136, 38);
+      }
+    }
   }
 }
 </style>
