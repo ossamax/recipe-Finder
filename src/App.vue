d5ed8a83f153c5e2f3cc3c0604d52994169d1cfe
ossama zaoute <o.zaoute@intel-think.com>
2024-04-09 17:22:42 +0000

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,81 @@
-<script setup>
+<script>
 import axios from "axios";
-import { onMounted, ref, watchEffect } from "vue";
-// import { data } from "../data";
+import { onMounted, ref, watch, watchEffect } from "vue";
 
-const inputValue = ref("");
-const data = ref([]);
-function GetData() {
-  axios
-    .get("https://api.spoonacular.com/recipes/findByIngredients", {
-      params: {
-        ingredients: inputValue.value ? inputValue.value : "chicken,pasta,drinks",
-        apiKey: "17cbab47fdf64d43b4e9f7661dd7ea66",
-      },
-    })
-    .then((response) => {
-      console.log(response.data);
-      data.value = response.data;
-    })
-    .catch((error) => {
-      this.error = error; // Store error in error variable
-      console.error("Error fetching recipes:", error);
-      this.loading = false; // Set loading state to false
+export default {
+  setup() {
+    const data = ref([]);
+    const inputValue = ref("");
+    const loading = ref(false);
+    const error = ref(null);
+
+    const fetchData = () => {
+      loading.value = true;
+
+      axios
+        .get("https://api.edamam.com/api/recipes/v2", {
+          params: {
+            type: "public",
+            q: inputValue.value.trim() ? inputValue.value.trim() : "all",
+            app_id: "56693293",
+            app_key: "b8f59935f387ea92845674ddcf84cafc",
+          },
+        })
+        .then((response) => {
+          data.value = response.data.hits;
+        })
+        .catch((error) => {
+          error.value = error;
+          console.error("Error fetching recipes:", error);
+        })
+        .finally(() => {
+          loading.value = false;
+        });
+    };
+
+    // Fetch data when component is mounted
+
+    const debounceFunction = debounce(fetchData, 600);
+    // onMounted(() => {
+    //   if (inputValue.value !== "") {
+    //     debounceFunction();
+    //   }
+    // });
+    watch(inputValue, () => {
+      if (inputValue.value !== "") {
+        debounceFunction();
+      }
     });
-}
 
-onMounted(GetData);
-watchEffect(() => {
-  GetData();
-}, [inputValue]);
+    return {
+      data,
+      inputValue,
+      loading,
+      error,
+    };
+  },
+};
+
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
 </script>
 
 <template>
   <main>
-    <div class="navigation">
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">Explore</a>
-        <a href="#">Community</a>
-        <a href="#">Favorites</a>
-        <a href="#">Help</a>
-        <a href="#">Settings</a>
-      </nav>
-    </div>
     <div class="wrapper">
       <header class="search_header">
+        <h1>Discover Recipes</h1>
+        <p>
+          Oh, no! You seem to have wandered into a food desert! You must be
+          hungry...
+        </p>
         <div class="input_wrp">
           <span
             ><svg
@@ -65,142 +97,102 @@ watchEffect(() => {
             placeholder="What do you want to cook today?"
           />
         </div>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            viewBox="0 0 24 24"
-          >
-            <g fill="none" stroke="currentColor" stroke-width="1.5">
-              <rect width="18.5" height="18.5" x="2.75" y="2.75" rx="6" />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.75 8.805h-1.437m-3.594 0H6.25m11.5 6.39h-6.469m-3.593 0H6.25m3.234 1.797a1.797 1.797 0 1 0 0-3.594a1.797 1.797 0 0 0 0 3.594m5.032-6.39a1.797 1.797 0 1 0 0-3.594a1.797 1.797 0 0 0 0 3.594"
-              />
-            </g>
-          </svg>
-        </button>
       </header>
-      <div class="intro">
-        <div class="intro_head">
-          <h1>Recommended Recipes</h1>
-          <p>Base on your preferences</p>
-        </div>
-        <div class="pagination">
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m14 7l-5 5l5 5"
-              />
-            </svg>
-          </button>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m10 17l5-5l-5-5"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-      <ul>
+      <ul v-if="data.length > 0">
         <li v-for="(item, index) in data" :key="index">
           <div class="likes-icon">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="15"
-              height="15"
+              width="20"
+              height="20"
               viewBox="0 0 256 256"
             >
               <path
                 fill="currentColor"
                 d="M178 36c-20.09 0-37.92 7.93-50 21.56C115.92 43.93 98.09 36 78 36a66.08 66.08 0 0 0-66 66c0 72.34 105.81 130.14 110.31 132.57a12 12 0 0 0 11.38 0C138.19 232.14 244 174.34 244 102a66.08 66.08 0 0 0-66-66m-5.49 142.36a328.69 328.69 0 0 1-44.51 31.8a328.69 328.69 0 0 1-44.51-31.8C61.82 159.77 36 131.42 36 102a42 42 0 0 1 42-42c17.8 0 32.7 9.4 38.89 24.54a12 12 0 0 0 22.22 0C145.3 69.4 160.2 60 178 60a42 42 0 0 1 42 42c0 29.42-25.82 57.77-47.49 76.36"
-              /></svg
-            >{{ item.likes }}
+              />
+            </svg>
           </div>
           <div class="img-bg">
-            <img :src="item.image" alt="image" class="card_img" />
+            <img :src="item.recipe.image" alt="image" class="card_img" />
           </div>
           <div class="content">
-            <h3>{{ item.title }}</h3>
-            <!-- <div class="ingredients-list">
-              <div v-for="(ingredient, key) in item.usedIngredients" :key="key">
-                {{ ingredient.name }}
+            <h3>{{ item.recipe.label }}</h3>
+            <div class="details">
+              <div class="time">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2M12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8m.5-13H11v6l5.25 3.15l.75-1.23l-4.5-2.67z"
+                  /></svg
+                >time
               </div>
-            </div> -->
-            <button>View Ingredients</button>
+              <div class="time">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                >
+                  <g fill="none">
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                    />
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-width="1.5"
+                      d="M9 16c.85.63 1.885 1 3 1s2.15-.37 3-1"
+                    />
+                    <path
+                      fill="currentColor"
+                      d="M16 10.5c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5s.448-1.5 1-1.5s1 .672 1 1.5"
+                    />
+                    <ellipse
+                      cx="9"
+                      cy="10.5"
+                      fill="currentColor"
+                      rx="1"
+                      ry="1.5"
+                    />
+                  </g></svg
+                >2 persons
+              </div>
+            </div>
+            <div class="reviews">
+              <div class="stars">
+                <div v-for="star in [1, 2, 3, 4, 5]" :key="star" class="star">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="orange"
+                      d="m12 16.3l-3.7 2.825q-.275.225-.6.213t-.575-.188t-.387-.475t-.013-.65L8.15 13.4l-3.625-2.575q-.3-.2-.375-.525t.025-.6t.35-.488t.6-.212H9.6l1.45-4.8q.125-.35.388-.538T12 3.475t.563.188t.387.537L14.4 9h4.475q.35 0 .6.213t.35.487t.025.6t-.375.525L15.85 13.4l1.425 4.625q.125.35-.012.65t-.388.475t-.575.188t-.6-.213z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              (20)
+            </div>
+            <!-- <button>View Ingredients</button> -->
           </div>
         </li>
       </ul>
-      <div class="intro">
-        <div class="intro_head">
-          <h1>Trending Recipes</h1>
-          <p>Cooked by peope with similar preferences</p>
-        </div>
-        <div class="pagination">
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m14 7l-5 5l5 5"
-              />
-            </svg>
-          </button>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m10 17l5-5l-5-5"
-              />
-            </svg>
-          </button>
-        </div>
+      <div v-if="data.length == 0" class="empty_img">
+        <img src="/empty_search.svg" width="400" height="300" alt="empty" />
       </div>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
     </div>
   </main>
 </template>
@@ -210,7 +202,16 @@ watchEffect(() => {
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 20px;
+  flex-direction: column;
+}
+p {
+  color: rgb(149, 147, 167);
+  font-weight: 300;
+}
+.empty_img {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .search_header span {
   display: flex;
@@ -219,32 +220,16 @@ watchEffect(() => {
   display: inline-flex;
   padding-inline-start: 1rem;
 }
-.search_header button {
-  background-color: lightseagreen;
-  color: #28455b;
-  box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
-  border-radius: 10px;
-  border: none;
-  width: 50px;
-  height: 48px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  color: white;
-  &:hover {
-    background-color: rgb(27, 156, 150);
-  }
-}
 .input_wrp {
   background-color: white;
-  box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
-  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+  border-radius: 25px;
   display: flex;
   align-items: center;
   width: 500px;
   gap: 10px;
   border: 1px solid transparent;
+  margin-top: 1rem;
   &:hover {
     border-color: lightseagreen;
   }
@@ -254,12 +239,12 @@ watchEffect(() => {
   height: 48px;
   border: none;
   padding: 0 0.5rem;
-  border-radius: 10px;
+  border-radius: 25px;
   font-size: 0.9rem;
   font-weight: 500;
   &::placeholder {
-    font-weight: 550;
-    color: rgb(149, 147, 167);
+    font-weight: 500;
+    color: rgb(132, 130, 151);
   }
 }
 .wrapper {
@@ -270,33 +255,7 @@ watchEffect(() => {
   display: flex;
   flex-direction: column;
   gap: 40px;
-  max-height: 100vh;
-  overflow-y: scroll;
 }
-.navigation {
-  background-color: white;
-  width: 18%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  nav {
-    display: flex;
-    flex-direction: column;
-    gap: 40px;
-    width: 100%;
-    padding-inline-start: 4rem;
-  }
-  a {
-    color: #28455b;
-    padding: 0rem 1rem;
-    text-decoration: none;
-    width: 100%;
-    font-size: 1.1rem;
-    border-inline-end: 2px solid gray;
-  }
-}
-
 main {
   min-height: 100vh;
   display: flex;
@@ -306,10 +265,7 @@ main {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  p {
-    color: rgb(149, 147, 167);
-    font-weight: 300;
-  }
+
   .pagination {
     display: flex;
     align-items: center;
@@ -337,17 +293,21 @@ ul {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   list-style: none;
-  gap: 20px;
+  gap: 25px;
   li {
-    background-color: white;
-    border-radius: 10px;
+    border-radius: 8px;
     position: relative;
     overflow: hidden;
-    box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+    background-color: white;
+    &:hover {
+      transform: translateY(-5px);
+    }
     .likes-icon {
-      background-color: rgb(252, 50, 83);
+      background-color: #28455b;
       color: white;
-      border-radius: 15px;
+      border-radius: 100%;
       display: flex;
       align-items: center;
       justify-content: center;
@@ -355,16 +315,44 @@ ul {
       position: absolute;
       top: 1rem;
       right: 1rem;
-      width: max-content;
-      padding: 0 1rem;
-      height: 35px;
+      width: 40px;
+      height: 40px;
       z-index: 100;
+    }
+    .reviews {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      font-weight: 400;
+      font-size: 0.8rem;
+    }
+    .stars {
+      display: flex;
+      align-items: center;
+    }
+    .star {
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
     .content {
       padding: 1rem;
       display: flex;
       flex-direction: column;
+      gap: 10px;
+    }
+    .details {
+      display: flex;
+      align-items: center;
       gap: 20px;
+      div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 400;
+        font-size: 0.8rem;
+        gap: 5px;
+      }
     }
     .img-bg {
       position: relative;
@@ -382,9 +370,11 @@ ul {
       width: 100%;
       z-index: 1;
       height: 100%;
+      border-radius: 5px;
     }
     h3 {
       color: black;
+      font-weight: 500;
     }
     .ingredients-list {
       display: flex;
@@ -400,15 +390,17 @@ ul {
     }
     button {
       width: 100%;
-      height: 45px;
-      background-color: rgb(248, 152, 43);
-      color: white;
+      height: 40px;
+      background-color: transparent;
+      color: lightseagreen;
       border-radius: 20px;
       font-weight: 600;
       border: none;
       cursor: pointer;
+      border: 1px solid lightseagreen;
       &:hover {
-        background-color: rgb(223, 136, 38);
+        background-color: lightseagreen;
+        color: white;
       }
     }
   }
